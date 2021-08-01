4a2f5a622be0c768025d226df0e13d2d1bd1201b
DaisukeHirabayashi <daisuke@pluslab.org>
2021-08-01 23:14:23 +0900

    テストコードの追加

--- test/jobs/async_log_job_test.rb
+++ test/jobs/async_log_job_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class AsyncLogJobTest < ActiveJob::TestCase
+  test "Enqueue AsymcLogJob" do
+    assert_enqueued_with(job: AsyncLogJob) do
+      AsyncLogJob.perform_later(message: "from test")
+    end
+  end
   # test "the truth" do
   #   assert true
   # end
