3ccf3e3c12cd22c6f1fd6393e5974d605e259e07
DaisukeHirabayashi <daisuke@pluslab.org>
2021-08-01 23:01:14 +0900

    ジョブのリトライ

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -7,4 +7,7 @@ class AsyncLogJob < ApplicationJob
       :default
     end
   end
+  retry_on StandardError, wait: 5.seconds, attempts: 3
+  # 複数の例外クラスを指定することも可能
+  #retry_on ArgumentError, ZeroDivisionError, wait:5.seconds. attempts: 3
 end
