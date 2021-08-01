38a4fa512140786b0ac6cde116909b0a93b01056
DaisukeHirabayashi <daisuke@pluslab.org>
2021-08-01 23:08:29 +0900

    ジョブを破棄する処理を追加

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -7,6 +7,9 @@ class AsyncLogJob < ApplicationJob
       :default
     end
   end
+  discard_on StandardError do |job, error|
+    SomeNotifier.push(error)
+  end
   retry_on StandardError, wait: 5.seconds, attempts: 3
   # 複数の例外クラスを指定することも可能
   #retry_on ArgumentError, ZeroDivisionError, wait:5.seconds. attempts: 3
